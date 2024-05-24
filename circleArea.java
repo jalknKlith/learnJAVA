f9f91567ae7dc3ae4076597a918d94e4628f4271
jalknKleith <169000561+jalknKleith@users.noreply.github.com>
2024-05-24 16:47:45 -0500

    Create circleArea.java

---
+++ circleArea.java
@@ -0,0 +1,18 @@
+/*This program calculates the area of a circle, reading the radius
+and printing the area.*/
+
+import java.util.Scanner;
+
+public class areaCirculo {
+
+    public static void main(String[] args){
+        
+        Scanner leer = new Scanner(System.in);
+        double radio, area;
+        System.out.println("Enter the radius");
+        radio = leer.nextDouble();
+        area = Math.PI * Math.pow(radio, 2);
+        System.out.println("The area is: "+area);
+
+    }
+}
