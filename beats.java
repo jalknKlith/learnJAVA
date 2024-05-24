9b0164b8580b9e0b4f280dd86f6c188013affe88
jalknKleith <169000561+jalknKleith@users.noreply.github.com>
2024-05-24 16:28:57 -0500

    Update beats.java

--- beats.java
+++ beats.java
@@ -1,8 +1,8 @@
-//Calculate the number of pulses
+//Calculate the number of beats
 //that a person should have for every
 //10 seconds of exercise, if the formula is:
 //... Number of Pulses = (220 - age)/10
-//... Read the age and print the number of pulses.
+//... Read the age and print the number of beats.
 
 import java.util.Scanner;
 
@@ -15,9 +15,9 @@ public class beats {
         int age = input.nextInt();
 
         //Calculating the number of pulses
-        double pulses = (220 - age) / 10.0;
+        double beats = (220 - age) / 10.0;
 
         //Print the number of pulses
-        System.out.println("The number of pulses per 10 seconds of exercise is: " + pulses);
+        System.out.println("The number of beats per 10 seconds of exercise is: " + beats);
     }
 }
