e32199bdcddc270a3a22f2281661e8bfc2f852f8
jalknKleith <169000561+jalknKleith@users.noreply.github.com>
2024-06-02 22:34:49 -0500

    Update and rename numerology.java to seria11for.java

---
+++ seria11for.java
@@ -0,0 +1,23 @@
+public class Main {
+        public static void main(String[] args) {
+                for (int x = 11; x <= 111; x+=11) {
+                System.out.println(x);
+}
+}
+}
+
+
+
+/*
+
+
+
+for x in range(2,9,2):
+        print(x)
+
+
+
+
+*/
+
+
