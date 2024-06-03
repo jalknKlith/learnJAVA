b5a0987bbe5073b9d239adb4f77fc1a229baaa31
jalknKleith <169000561+jalknKleith@users.noreply.github.com>
2024-06-02 22:32:56 -0500

    Update squarea.java

--- squarea.java
+++ squarea.java
@@ -3,17 +3,26 @@ import java.util.Scanner;
 public class squarea {
 
         public static void main(String[] args) {
+                // Ask for the side
+                int side, area;
+                Scanner input = new Scanner(System.in);
+                System.out.println("Enter the length of the side");
+                side = input.nextInt();
 
-        // Ask for the side
-        int side, area;
-        Scanner read = new Scanner(System.in);
-        System.out.println("Enter the length of the side");
-        side = read.nextInt();
+                // Calculate the area of a rectangle
+                area = (int) Math.pow(side, 2);
 
-        // Calculate the area of the square
-        area = (int) Math.pow(side,2);
-
-        // Print variables the result
-        System.out.println("The area of the square is: " + area);
-  }
+                // Print variables the result
+                System.out.println("The area of the square is: " + area);
+    }
 }
+
+/*
+side = float(input("Enter the length of the side"))
+
+area = side ** 2
+
+print("The area of the square is:", area)
+
+
+*/
