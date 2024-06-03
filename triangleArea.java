75cde28457d117e0831b21be611030bda72515fe
jalknKleith <169000561+jalknKleith@users.noreply.github.com>
2024-06-02 22:26:28 -0500

    Update triangleArea.java
    new comments with python codes

--- triangleArea.java
+++ triangleArea.java
@@ -1,25 +1,35 @@
 /* calculate the area of a triangle,
 read the base, the height and print the result */
 
-import java.util.Scanner; 
+import java.util.Scanner;
 
 public class triangleArea {
 
-    public static void main(String[] args) {
-        
-        float base, height, area;
-        Scanner read = new Scanner(System.in);
-        //base length
-        System.out.println("Enter the base of the triangle");
-        base = read.nextFloat(); //implicit conversion
-        //height
-        System.out.println("Enter the height of the triangle");
-        height = (float) read.nextFloat(); //explicit conversion
-        area = base * height / 2f;
-        //print area
-        System.out.println("The area is: "+area);
+        public static void main(String[] args) {
+
+                float base, height, area;
+                Scanner input = new Scanner(System.in);
+                System.out.println("Enter the base of the triangle");
+                base = input.nextFloat();
+                System.out.println("Enter the height of the triangle");
+                height = input.nextFloat();
+                area = base * height / 2f;
+                System.out.println("The area is: "+area);
 
 
     }
 
 }
+
+/* python
+
+base = float(input("Enter the base of the triangle: "));
+height = float(input("Enter the height of the triangle: "));
+
+area = (base * height) / 2
+
+print("The area is:",area)
+
+*/
+
+
