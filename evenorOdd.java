4b356ea1491f5698982da33b5513b6119595c6b0
jalknKleith <169000561+jalknKleith@users.noreply.github.com>
2024-06-02 22:28:23 -0500

    Update evenorOdd.java
    new comments wit python code

--- evenorOdd.java
+++ evenorOdd.java
@@ -1,19 +1,34 @@
 import java.util.Scanner;
 
-public class Main {
+public class evenOdd {
+
         public static void main(String[] args) {
-        
                 //Ask for number
                 Scanner sc = new Scanner(System.in);
-                int number;
                 System.out.println("Write a number");
-                number = sc.nextInt();
-                
+                int x = sc.nextInt();
+
                 // Find out if the number above is even or odd
-                if (number % 2 == 0) {
-                System.out.println(number + " is even.");
+                if (x % 2 == 0) {
+                        System.out.println(x + " is even.");
                 } else {
-                System.out.println(number + " is odd.");
+                        System.out.println(x + " is odd.");
+        }
+    }
 }
-}
-}
+
+
+
+/*
+
+
+number = int(input("Write a number: "))
+
+if number % 2 == 0:
+        print(x, "is even")
+else:
+        print(x, "is ood")
+
+
+
+*/
