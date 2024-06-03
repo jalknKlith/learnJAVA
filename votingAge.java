fe6932252f52ef897f13f9813095de8d5a034c57
jalknKleith <169000561+jalknKleith@users.noreply.github.com>
2024-06-03 18:26:12 -0500

    Update votingAge.java
    comments with python code

--- votingAge.java
+++ votingAge.java
@@ -1,19 +1,17 @@
 import java.util.Scanner;
 
 public class votingAge {
- 
-    public static void main(String[] args) {
 
-            Scanner input = new Scanner(System.in) {
-            System.out.println("Enter your age");
-            int age = input.nextInt();
+        public static void main(String[] args) {
+                Scanner input = new Scanner(System.in);
 
-            if (age >= 18) {
-                System.out.println("Old enough to vote!");
-            } else {
-                System.out.println("Not old enough to vote.");
-            }
+                System.out.println("Enter your age");
+                int age = input.nextInt();
 
+                if (age >= 18) {
+                        System.out.println("Old enough to vote!");
+                } else {
+                        System.out.println("Not old enough to vote.");
         }
     }
 }
@@ -21,9 +19,10 @@ public class votingAge {
 /*
 
 age = int(input("Enter your age"))
+
 if age >= 18:
-    print("Old enough to vote!")
+        print("Old enough to vote!")
 else:
-    print("Not old enough to vote!")
-    
+        print("Not old enough to vote.")
+
 */
